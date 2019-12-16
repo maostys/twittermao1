97e0f5b6d0ded612f151fe589c52509e3505d4bb
maostys <57489588+maostys@users.noreply.github.com>
2019-12-16 12:04:42 +0900

    Add files via upload

--- Twitterbot01/src/main/java/HelloAppEngine.java
+++ Twitterbot01/src/main/java/HelloAppEngine.java
@@ -1,24 +1,60 @@
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(
-    name = "HelloAppEngine",
-    urlPatterns = {"/hello"}
-)
-public class HelloAppEngine extends HttpServlet {
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) 
-      throws IOException {
-
-    response.setContentType("text/plain");
-    response.setCharacterEncoding("UTF-8");
-
-    response.getWriter().print("Hello App Engine!\r\n");
-
-  }
+public class HelloAppEngine extends HttpServlet{
+	static Logger logger = Logger.getLogger("SenyaBot");
+	
+	static String consumerKey = "xxxxxx";
+	static String consumerSecret = "xxxxxx";
+	
+	static String accessToken = "xxxxx";
+	static String accessTokenSecret = "xxxxx";
+	
+	private String getTweet(){
+		String tweets[] = {
+				"家系といったら千屋っしょ",
+				"通は固めの濃いめ",
+				"やっぱり根岸の千家がいいよね。",
+				"信頼の深夜営業",
+				"水曜は定休日なんだよな",
+				"とりあえず千家行こうぜ",
+				"ユー、ライス付けちゃう？",
+				"ミニカレーも意外と美味しいぜ",
+				"ネギうますぎ",
+				"あのネギ盛見るとテンションあがっちゃうよねー",
+				"大将まじかっけぇ",
+				"ここは食券制です"
+		};
+		int randint = (int)(Math.random()*tweets.length);
+		return tweets[randint];
+	}
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		//http://twitter4j.org/ja/configuration.html
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setDebugEnabled(true)
+			.setOAuthAccessToken(accessToken)
+			.setOAuthAccessTokenSecret(accessTokenSecret)
+			.setOAuthConsumerKey(consumerKey)
+			.setOAuthConsumerSecret(consumerSecret);
+		String message = getTweet();
+		Twitter twitter = new TwitterFactory(cb.build()).getInstance();
+		try {
+			//Twitterに書き出し
+			twitter.updateStatus(message);
+		} catch (TwitterException e) {
+			logger.log(Level.SEVERE, "Twitter error", e);
+		}
+	}
 }
